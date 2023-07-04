77bfb2e13919c5f2557db1e00b1867774cc1d90e
Software Shinobi <45909594+software-shinobi@users.noreply.github.com>
2023-07-04 10:06:32 -0500

    Update ubuntu-server-provision.sh

--- source-code/server-provisioning/ubuntu-server-provision.sh
+++ source-code/server-provisioning/ubuntu-server-provision.sh
@@ -12,17 +12,17 @@ cat /etc/issue
 
 apt-get update;
 
-apt-get install vim -y;
+apt-get install -y vim;
 
-apt-get install cmatrix -y;
+apt-get install -y cmatrix;
 
-#apt-get install hollywood -y;
+apt-get install -y hollywood;
 
 ##
 
-apt-get install openssh-server -y;
+apt-get install -y openssh-server;
 
-apt-get install sudo -y;
+apt-get install -y sudo;
 
 useradd -rm -d /home/linux -s /bin/bash -u 4444 linux;
 
