b7dff0a43a91e8c5e5faf3fa08a0d317a1b5fb05
Software Shinobi <the.software.shinobi@gmail.com>
2024-07-26 08:09:45 -0500

    automated terminal push

--- provision/provision.bash
+++ provision/provision.bash
@@ -19,7 +19,7 @@ set -e;
 
 set -x;
 
-## 
+## setting server hostname
 
 cat /etc/issue;
 
@@ -27,48 +27,51 @@ echo "Shinobi OS 22.04" > /etc/issue;
 
 cat /etc/issue;
 
-##
+## updating APT software packages
 
 apt-get update;
 
-apt-get install docker-compose -y;
 ##
 
-apt-get install -y vim;
+sudo apt-get install ca-certificates curl;
 
-##
+## Installing Docker + Compose (Not Docker Compose)
+
+
+### APT package dependency setup
+
+sudo install -m 0755 -d /etc/apt/keyrings
+
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+### Docker Engine situation install
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo docker run hello-world
+
+## Install more software
+
+apt-get install -y vim zip wget net-tools;
+
+## install and start a local ssh server
 
 apt-get install -y openssh-server;
 
 apt-get install -y sudo;
 
-##
-
 service ssh start;
 
-##
+## setting up users and groups
 
-userName="shinobi";
-
-userID="4444";
-
-##
-
-##mkdir /home/users;
-
-useradd -rm -d /home/$userName -s /bin/bash -u $userID $userName;
-
-#chown root:root /home/$userName -R
-
-usermod -a -G sudo shinobi
-
-usermod -a -G docker shinobi
-
-##echo '$userName:$userName' | chpasswd
-
-## make this work later. moving on. // echo '`$userName`:`$userName`' | chpasswd
-
-echo 'shinobi:shinobi' | chpasswd
+bash users.bash
 
 ##
 
