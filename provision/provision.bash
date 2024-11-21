4ec9f1a80028334ad9f27ffcdce6781fb1b4b3d3
Software Shinobi <the.software.shinobi@gmail.com>
2024-11-21 10:07:45 -0500

    automated terminal push

--- provision/provision.bash
+++ provision/provision.bash
@@ -53,7 +53,7 @@ echo \
 
 ### Docker Engine situation install
 
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 docker run hello-world
 
